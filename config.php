25f022229e51d4b9572b41f5da0faa2627647122
anfeket <anfeket@gmail.com>
2025-10-25 01:21:21 +0200

    Fixed last change date

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $build_file = __DIR__ . '/build.txt';
 if (file_exists($build_file)) {
-	define('LAST_CHANGE', file($build_file));
+	define('LAST_CHANGE', file_get_contents($build_file));
 } else {
 	define('LAST_CHANGE', 'nie je zaznamenané');
 }
