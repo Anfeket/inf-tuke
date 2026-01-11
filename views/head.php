03d6c31ede0fac55496f114bc69f87643d9882a6
anfeket <n5ye.yt@gmail.com>
2026-01-11 19:37:32 +0100

    ups

--- views/head.php
+++ views/head.php
@@ -2,7 +2,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="/styles.css?v=<?= BUILD_COMMIT ?? 'dev' ?>">
-	<link rel="shortcut icon" href="https://f35.svidnik.org//assets/f35-plain-white.svg" type="image/x-icon">
+	<link rel="shortcut icon" href="https://f35.svidnik.org/assets/f35-plain-white.svg" type="image/x-icon">
 	<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Inter&display=swap" rel="stylesheet">
 	<title>F35</title>
 </head>
